14a377a45447ac644326d395d31fbf0f76b8f3c9
abpy <aaronbuchler@aol.com>
2015-11-22 20:32:20 -0500

    fix error and update url

--- img overlay.py
+++ img overlay.py
@@ -2,13 +2,13 @@ import urllib2
 import time
 import sys
 
-def download_pdf_to_jpg(county, name, year):
+def download_pdf_to_jpg(county, year, name):
     if county == "wayne":
         part = "part2/"
     else:
         part = "part1/"
-    pdf = urllib2.urlopen("http://www.clas.wayne.edu/photos/" + part + county + "/" + year + "/" + name + ".pdf")
-    print "downloading", name
+    pdf = urllib2.urlopen("http://claslinux.clas.wayne.edu/photos/" + part + county + "/" + year + "/" + name + ".pdf")
+    print "downloading", name, "..."
     t1 = time.time()
     pdfimg = pdf.read()
     pdf.close()
@@ -32,40 +32,45 @@ def download_pdf_to_jpg(county, name, year):
     print t2 - t1
     print "done"
 
+def image_overlay_kml(county, year, name):
+    countydir = county + " index maps/"
+
+    #get location
+    imgfile = open(countydir + year + county + ".txt", "r")
+    imglist = imgfile.readlines()
+    imgfile.close()
+
+    imgdict = {}
+    for i in imglist:
+        i = i[:-1].split(",")
+        imgdict[i[0]] = (float(i[1]), float(i[2]))
+
+    location = imgdict[name]
+
+    # dimensions in degrees of a photo
+    height = 0.027895
+    width = 0.039518
+
+    #geographic bounds
+    north = location[0] + (height / 2)
+    south = location[0] - (height / 2)
+    east = location[1] + (width / 2)
+    west = location[1] - (width / 2)
+
+    #image overlay
+    kmlf = open("overlay_template.txt", "r")
+    template = kmlf.read()
+    kmlf.close()
+
+    overlaykml = template % (name + "overlay", location[1], location[0], "../photographs/"+ name + ".jpg", north, south, east, west)
+
+    file = open("kml overlays/" + name + "overlay.kml", "w")
+    file.write(overlaykml)
+    file.close()
+
 county = raw_input("county: ")
 year = raw_input("year: ")
 name = raw_input("name: ")
-download_pdf_to_jpg(county, name, year)
 
-countydir = county + " index maps/"
-
-imgfile = open(countydir + year + county + ".txt", "r")
-imglist = imgfile.readlines()
-imgfile.close()
-
-imgdict = {}
-for i in imglist:
-    i = i[:-1].split(",")
-    imgdict[i[0]] = (float(i[1]), float(i[2]))
-
-location = imgdict[name]
-
-# dimensions in degrees of a photo
-height = 0.027895
-width = 0.039518
-
-north = location[0] + (height / 2)
-south = location[0] - (height / 2)
-east = location[1] + (width / 2)
-west = location[1] - (width / 2)
-
-#image overlay
-kmlf = open("overlay_template.txt", "r")
-template = kmlf.read()
-kmlf.close()
-
-overlaykml = template % (name + "overlay", location[1], location[0], "../photographs/"+ name + ".jpg", north, south, east, west)
-
-file = open("kml overlays/" + name + "overlay.kml", "w")
-file.write(overlaykml)
-file.close()
+download_pdf_to_jpg(county, year, name)
+image_overlay_kml(county, year, name)
